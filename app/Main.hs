32c6c163d332e64b28d1022d787cf7e664b49463
Alexandru Ghitza <aghitza@alum.mit.edu>
2016-05-20 20:39:05 +1000

    include simple code illustrating the use of HLinear to compute PLE decomposition of a matrix

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
 import Lib
+import HFlint.FMPQ
+import HLinear.Matrix as M
+import HLinear.PLE.FoldUnfold as FU
+import HLinear.PLE.Decomposition as D
+import HLinear.PLE.Hook as Hk
 
 main :: IO ()
-main = someFunc
+main = do
+  let m = M.fromListsUnsafe [[1,2,3],[4,5,6]] :: Matrix FMPQ
+      hk = D.unPLEDecomposition $ FU.pleDecompositionFoldUnfold m
+      p = Hk.permutation hk
+      l = Hk.leftTransformation hk
+      e = Hk.echelonForm hk
+  print p
+  print l
+  print e

39a32395b1fc0c758fc9a9bdbbcba0e3981b4957
Alexandru Ghitza <aghitza@alum.mit.edu>
2016-05-21 07:58:31 +1000

    print P, L, E as matrices

--- app/Main.hs
+++ app/Main.hs
@@ -6,14 +6,23 @@ import HLinear.Matrix as M
 import HLinear.PLE.FoldUnfold as FU
 import HLinear.PLE.Decomposition as D
 import HLinear.PLE.Hook as Hk
+import HLinear.PLE.Hook.EchelonForm as EF
+import HLinear.PLE.Hook.RPermute as RP
+import HLinear.PLE.Hook.LeftTransformation as LT
 
 main :: IO ()
 main = do
   let m = M.fromListsUnsafe [[1,2,3],[4,5,6]] :: Matrix FMPQ
       hk = D.unPLEDecomposition $ FU.pleDecompositionFoldUnfold m
       p = Hk.permutation hk
+      pm = RP.toMatrix p :: Matrix FMPQ
       l = Hk.leftTransformation hk
+      lm = LT.toMatrix l
       e = Hk.echelonForm hk
-  print p
-  print l
-  print e
+      em = EF.toMatrix e
+  putStrLn "permutation:"
+  print pm
+  putStrLn "left transformation:"
+  print lm
+  putStrLn "echelon form:"
+  print em

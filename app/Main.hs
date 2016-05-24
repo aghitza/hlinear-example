4b687f2f39a1f3cc159037d4c38ce94ed40bf37c
Alexandru Ghitza <aghitza@alum.mit.edu>
2016-05-24 12:31:54 +1000

    go even higher level

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,12 @@ module Main where
 import Lib
 import HFlint.FMPQ
 import HLinear.Matrix as M
-import HLinear.PLE.FoldUnfold as FU
 import HLinear.PLE.Decomposition as D
 
 main :: IO ()
 main = do
   let m = M.fromListsUnsafe [[1,2,3],[4,5,6]] :: Matrix FMPQ
-      (pm, lm, em) = D.toMatrices $ FU.pleDecompositionFoldUnfold m
+      (pm, lm, em) = D.toMatrices $ D.pleDecomposition m
   putStrLn "permutation:"
   print pm
   putStrLn "left transformation:"
